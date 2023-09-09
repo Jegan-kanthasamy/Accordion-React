955afb7cb77d69bc3659fb8559ecb786d1144cd2
Jegan-kanthasamy <jegankanthasamy@outlook.com>
2023-09-09 18:22:10 +0530

    Add Open and close fuctionality

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import "../index.css";
 import AccordionItem from "./AccordionItem";
+
 export default function Accordion({ details }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {details.map((e, i) => (
-        <AccordionItem num={i} title={e.title} text={e.text} />
+        <AccordionItem
+          num={i}
+          title={e.title}
+          text={e.text}
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+        />
       ))}
     </div>
   );
--- src/components/AccordionItem.js
+++ src/components/AccordionItem.js
@@ -1,18 +1,20 @@
-import { useState } from "react";
 import "../index.css";
-export default function AccordionItem({ num, title, text }) {
-  const [toggle, setToggle] = useState(true);
+
+export default function AccordionItem({ num, title, text, curOpen, onOpen }) {
+  const toggle = num === curOpen;
+
+  function handleToggle() {
+    onOpen(toggle ? null : num);
+  }
+
   return (
-    <div
-      className={`item ${toggle ? "" : "open"}`}
-      onClick={e => setToggle(!toggle)}
-    >
-      <p className={`number ${toggle ? "" : "color"} `}>
+    <div className={`item ${toggle ? "open" : ""}`} onClick={handleToggle}>
+      <p className={`number ${toggle ? "color" : ""} `}>
         {num > 8 ? num + 1 : `${0}${num + 1}`}
       </p>
-      <p className={`title ${toggle ? "" : "color"} `}>{title}</p>
+      <p className={`title ${toggle ? "color" : ""} `}>{title}</p>
       <p className="icon">{toggle ? "+" : "-"}</p>
-      <div className={`content-box ${toggle ? "disable" : ""}`}>
+      <div className={`content-box ${toggle ? "" : "disable"}`}>
         <p>{text}</p>
       </div>
     </div>
